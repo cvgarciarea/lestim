97f84ab496407258b3bddc081643516a8b12cc79
Cristian García <cristian99garcia@gmail.com>
2015-05-14 17:13:14 -0300

    Arreglo menor en el visor de aplicaciones

--- panel/panel.py
+++ panel/panel.py
@@ -232,6 +232,8 @@ class LestimPanel(Gtk.Window):
         G.run_app(button.app)
 
     def __reveal(self):
+        self.entry.set_text('')
+
         def move():
             x, y = self.get_position()
             w, h = self.get_size()
